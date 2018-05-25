9364371757f493ee5f68548bb1b181164c0283ad
Connor Wolfe <cbw36@case.edu>
2018-05-25 15:36:17 -0400

    read/write

---
+++ Tarea 3/helperFunctions.h
@@ -0,0 +1,15 @@
+#ifndef TAREA_3_HELPERFUNCTIONS_H
+#define TAREA_3_HELPERFUNCTIONS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+
+char* hex_to_bin(char hex);
+int * dec_to_bin(int dec_name, int*bin_name, int ind);
+int findNextAvailable();
+
+
+#endif //TAREA_3_HELPERFUNCTIONS_H
